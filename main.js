c0e27b0147703035095cd29803adea426408753a
Parviz_8901 <parvizakmalov39@gmail.com>
2024-04-29 23:52:07 +0500

    lesson js homework 18

---
+++ main.js
@@ -0,0 +1,143 @@
+const left1 = document.querySelector(".leftelement1")
+const left2 = document.querySelector(".leftelement2")
+const left3 = document.querySelector(".leftelement3")
+const left4 = document.querySelector(".leftelement4")
+const left5 = document.querySelector(".leftelement5")
+const left6 = document.querySelector(".leftelement6")
+const left7 = document.querySelector(".leftelement7")
+const left8 = document.querySelector(".leftelement8")
+const left9 = document.querySelector(".leftelement9")
+const left10 = document.querySelector(".leftelement10")
+
+
+const right1 = document.querySelector(".rightelement1")
+const right2 = document.querySelector(".rightelement2")
+const right3 = document.querySelector(".rightelement3")
+const right4 = document.querySelector(".rightelement4")
+const right5 = document.querySelector(".rightelement5")
+const right6 = document.querySelector(".rightelement6")
+const right7 = document.querySelector(".rightelement7")
+const right8 = document.querySelector(".rightelement8")
+const right9 = document.querySelector(".rightelement9")
+const right10 = document.querySelector(".rightelement10")
+
+left1.onclick = () => {
+    right1.style.display = "block"
+    right2.style.display = "none"
+    right3.style.display = "none"
+    right4.style.display = "none"
+    right5.style.display = "none"
+    right6.style.display = "none"
+    right7.style.display = "none"
+    right8.style.display = "none"
+    right9.style.display = "none"
+    right10.style.display = "none"
+}
+left2.onclick = () => {
+    right2.style.display = "block"
+    right1.style.display = "none"
+    right3.style.display = "none"
+    right4.style.display = "none"
+    right5.style.display = "none"
+    right6.style.display = "none"
+    right7.style.display = "none"
+    right8.style.display = "none"
+    right9.style.display = "none"
+    right10.style.display = "none"
+}
+left3.onclick = () => {
+    right3.style.display = "block"
+    right2.style.display = "none"
+    right1.style.display = "none"
+    right4.style.display = "none"
+    right5.style.display = "none"
+    right6.style.display = "none"
+    right7.style.display = "none"
+    right8.style.display = "none"
+    right9.style.display = "none"
+    right10.style.display = "none"
+}
+left4.onclick = () => {
+    right4.style.display = "block"
+    right2.style.display = "none"
+    right3.style.display = "none"
+    right1.style.display = "none"
+    right5.style.display = "none"
+    right6.style.display = "none"
+    right7.style.display = "none"
+    right8.style.display = "none"
+    right9.style.display = "none"
+    right10.style.display = "none"
+}
+left5.onclick = () => {
+    right5.style.display = "block"
+    right2.style.display = "none"
+    right3.style.display = "none"
+    right4.style.display = "none"
+    right1.style.display = "none"
+    right6.style.display = "none"
+    right7.style.display = "none"
+    right8.style.display = "none"
+    right9.style.display = "none"
+    right10.style.display = "none"
+}
+left6.onclick = () => {
+    right6.style.display = "block"
+    right2.style.display = "none"
+    right3.style.display = "none"
+    right4.style.display = "none"
+    right5.style.display = "none"
+    right1.style.display = "none"
+    right7.style.display = "none"
+    right8.style.display = "none"
+    right9.style.display = "none"
+    right10.style.display = "none"
+}
+left7.onclick = () => {
+    right7.style.display = "block"
+    right2.style.display = "none"
+    right3.style.display = "none"
+    right4.style.display = "none"
+    right5.style.display = "none"
+    right6.style.display = "none"
+    right1.style.display = "none"
+    right8.style.display = "none"
+    right9.style.display = "none"
+    right10.style.display = "none"
+}
+left8.onclick = () => {
+    right8.style.display = "block"
+    right2.style.display = "none"
+    right3.style.display = "none"
+    right4.style.display = "none"
+    right5.style.display = "none"
+    right6.style.display = "none"
+    right7.style.display = "none"
+    right1.style.display = "none"
+    right9.style.display = "none"
+    right10.style.display = "none"
+}
+left9.onclick = () => {
+    right9.style.display = "block"
+    right2.style.display = "none"
+    right3.style.display = "none"
+    right4.style.display = "none"
+    right5.style.display = "none"
+    right6.style.display = "none"
+    right7.style.display = "none"
+    right8.style.display = "none"
+    right1.style.display = "none"
+    right10.style.display = "none"
+}
+left10.onclick = () => {
+    right10.style.display = "block"
+    right2.style.display = "none"
+    right3.style.display = "none"
+    right4.style.display = "none"
+    right5.style.display = "none"
+    right6.style.display = "none"
+    right7.style.display = "none"
+    right8.style.display = "none"
+    right9.style.display = "none"
+    right1.style.display = "none"
+}
